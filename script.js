743c7431566e50b2499e76e4e3cd11dc838c11a5
Blake <blake.p.preston@gmail.com>
2021-01-03 22:18:34 -0600

    Adding initial work done

---
+++ script.js
@@ -0,0 +1,40 @@
+function showTime() {
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+
+  var time = h + ":" + m + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
+
+/***************************/
+
+document.getElementById("add").onclick = function(){
+  //get the text to add to list
+  var node = document.createElement("Li")
+  var text = document.getElementById("input").value;
+  var textnode = document.createTextNode(text);
+
+  node.appendChild(textnode);
+
+  //appendChild allows to add to the list
+  document.getElementById("list").appendChild(node);
+}
